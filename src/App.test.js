809d2271b7311b3aff8a60a22b91f8e84e57ceb0
Huseyin Cem Akyuz <hsyncmakyz@gmail.com>
2021-11-24 16:55:15 +0300

    test finished

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
+import Header from "./Header";
 
-test("renders without crashing", () => {
+
+it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
-}); 
-   
-beforeEach(() => {
-  render(<Header/>); 
-
 });
+
+test("Başlık bulundu", () => {
+  const div2 = document.createElement("div");
+  ReactDOM.render(<Header />, div2);
+});
+
+
+test("Emojiler Listelendi", () => {
+  render(<App />);
+  const emojiListContainer = document.querySelector(".component-emoji-results");
+  expect(emojiListContainer.childNodes.length === 20);
+});
+
+
+test("İnput ile Emoji filtrelendi", () => {
+  render(<App />);
+  const inputElement = document.getElementsByTagName("input");
+  userEvent.type(inputElement[0], "smile");
+  expect(screen.getByText("Grinning"))
+});
+
+test("Kopyalama tuşu kontrol edildi",()=>{
+  render(<App />);
+  const clickedItem = document.querySelector(".component-emoji-results")
+  userEvent.click(clickedItem)
+  
+})
+
+
+ 
+   
+
+
+ 
+
+
+
+
+ 
